87b95ae930cc62c326fc898a874c53b7225fcee9
zhaopapers <138082329+zhaopapers@users.noreply.github.com>
2024-08-30 16:10:40 +0800

    Update plot_cell_infiltration.R

--- experiment_data/plot_cell_infiltration.R
+++ experiment_data/plot_cell_infiltration.R
@@ -1,1 +1,11 @@
+#SDY144 
+GSE52005<-read.delim("GSE52005_symbol_aggregate_20000.csv")
+library(immunedeconv)
+tcga_quantiseq<-deconvolute(as.matrix(GSE52005),"quantiseq")
+tcga_mcp<-deconvolute(GSE52005,"mcp_counter")
+tcga_epic<-deconvolute(GSE52005,"epic")
+deco_xcell<-deconvolute(GSE52005,"xcell")
+tcga_abis<-deconvolute(GSE52005,"abis")
+lnscore_GSE52005<-lnscore(GSE52005)
 
+load()
