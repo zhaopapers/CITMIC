103862c2788467d6e3b0043546b768db3a7adcc2
zhaopapers <138082329+zhaopapers@users.noreply.github.com>
2024-09-02 20:59:03 +0800

    Update plot_correlation_pheatmap.R

--- experiment_data/plot_correlation_pheatmap.R
+++ experiment_data/plot_correlation_pheatmap.R
@@ -1,14 +1,13 @@
 #correlation pheatmap
-
+lnScore_SKCM<-CITMIC(exp_fpkm_tumor_aggregate_log2_20000,cl.cores = 8)
+lnScore_SKCM
 cell_inter<-data.frame(matrix(0,nrow=86,ncol=86))
-colnames(cell_inter)<-rownames(Inscore_SKCM)
-rownames(cell_inter)<-rownames(Inscore_SKCM)
+colnames(cell_inter)<-rownames(lnScore_SKCM)
+rownames(cell_inter)<-rownames(lnScore_SKCM)
 
-
-result<-c()
 for(i in 1:86){
   for(j in 1:86){
-    cor<-cor.test(Inscore_SKCM[i,],Inscore_SKCM[j,],method="pearson")
+    cor<-cor.test(lnScore_SKCM[i,],lnScore_SKCM[j,],method="pearson")
     if(cor[["p.value"]]<0.05){
       cell_inter[i,j]<-cor[["estimate"]][["cor"]]
       
@@ -20,12 +19,7 @@ for(i in 1:86){
 
 
 library(pheatmap)
-
-
 my_colors <- colorRampPalette(c("#5394cd","white", "#c12e34"))(100)
-
-
 pheatmap(cell_inter, 
          color = my_colors,
-         breaks = seq(-1, 1, length.out = 101))  # breaks 设置颜色映射的范围，使 0 为白色
-pheatmap(cell_inter)
+         breaks = seq(-1, 1, length.out = 101))  
