0bb40d2ad0a235b5c8bdf8013689e9e2a8f16374
zhaopapers <138082329+zhaopapers@users.noreply.github.com>
2024-09-02 20:54:03 +0800

    Create plot_correlation_pheatmap.R

---
+++ experiment_data/plot_correlation_pheatmap.R
@@ -0,0 +1,34 @@
+#correlation pheatmap
+cell_inter<-data.frame(matrix(0,nrow=86,ncol=86))
+colnames(cell_inter)<-rownames(Inscore_SKCM)
+rownames(cell_inter)<-rownames(Inscore_SKCM)
+
+
+library(corrgram)
+library(corrplot)
+
+corrplot(t(network_cell_score), order = "hclust")
+result<-c()
+for(i in 1:86){
+  for(j in 1:86){
+    cor<-cor.test(network_cell_score[i,],network_cell_score[j,],method="pearson")
+    if(cor[["p.value"]]<0.05){
+      cell_inter[i,j]<-cor[["estimate"]][["cor"]]
+      
+    }
+    
+  }
+  
+}
+
+
+library(pheatmap)
+
+
+my_colors <- colorRampPalette(c("#5394cd","white", "#c12e34"))(100)
+
+
+pheatmap(cell_inter, 
+         color = my_colors,
+         breaks = seq(-1, 1, length.out = 101))  # breaks 设置颜色映射的范围，使 0 为白色
+pheatmap(cell_inter)
