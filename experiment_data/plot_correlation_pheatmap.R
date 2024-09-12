ec6f8c999ba13860d32cb4feee33231b97aee33f
zhaopapers <138082329+zhaopapers@users.noreply.github.com>
2024-09-12 09:55:25 +0800

    Update plot_correlation_pheatmap.R

--- experiment_data/plot_correlation_pheatmap.R
+++ experiment_data/plot_correlation_pheatmap.R
@@ -1,12 +1,12 @@
 #correlation pheatmap
 lnScore_SKCM<-CITMIC(exp_fpkm_tumor_aggregate_log2_20000,cl.cores = 8)
 lnScore_SKCM
-cell_inter<-data.frame(matrix(0,nrow=86,ncol=86))
+cell_inter<-data.frame(matrix(0,nrow=71,ncol=71))
 colnames(cell_inter)<-rownames(lnScore_SKCM)
 rownames(cell_inter)<-rownames(lnScore_SKCM)
 
-for(i in 1:86){
-  for(j in 1:86){
+for(i in 1:71){
+  for(j in 1:71){
     cor<-cor.test(lnScore_SKCM[i,],lnScore_SKCM[j,],method="pearson")
     if(cor[["p.value"]]<0.05){
       cell_inter[i,j]<-cor[["estimate"]][["cor"]]
