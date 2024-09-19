dfcde4be629dcb86877244e3f8de97e8799efb33
zhaopapers <138082329+zhaopapers@users.noreply.github.com>
2024-09-19 09:38:43 +0800

    Update plot_correlation_pheatmap.R

--- experiment_data/plot_correlation_pheatmap.R
+++ experiment_data/plot_correlation_pheatmap.R
@@ -23,3 +23,34 @@ my_colors <- colorRampPalette(c("#5394cd","white", "#c12e34"))(100)
 pheatmap(cell_inter, 
          color = my_colors,
          breaks = seq(-1, 1, length.out = 101))  
+
+#View the top 500 bp most relevant to the subgroup
+cell_1<-c("Th1 cells","CTLs","Cytotoxic cells","Activated CD8+ T cells",
+          "nTreg","DCs","Tregs","Tr1","T cells","iTregs","Immature  B cells",
+          "Activated CD4+ T cells","Th17 cells","Tfh","MDSCs")
+cell_2<-c("Pericyte","mv Endothelial cells","Smooth muscle cell",
+          "Plasma cells","MSCs","Preadipocytes","Muscle cell","Platelets",
+          "CD4+ memory T cells")
+
+sum_r<-data.frame(apply(cell_1_go,1,sum))
+sum_r<-cbind(rownames(sum_r),sum_r[,1])
+sum_r<-data.frame(sum_r)
+sum_r[,2]<-as.numeric(sum_r[,2])
+sum_r<-sum_r[order(sum_r[,2],decreasing = T),]
+
+edge_list_1<-melt(cell_1_go[sum_r[1:46,1],])#The bp with the highest score in the subgroup is the bp that best represents the correlation within the group
+edge_list_1<-edge_list_1[order(edge_list_1[,3],decreasing = T),]
+edge_list_1<-edge_list_1[1:500,]
+write.csv(edge_list_1,"edge_list_1.csv")
+
+cell_2_go<-matrix_cell_go_score[,cell_2]
+sum_r<-data.frame(apply(cell_2_go,1,sum))
+sum_r<-cbind(rownames(sum_r),sum_r[,1])
+sum_r<-data.frame(sum_r)
+sum_r[,2]<-as.numeric(sum_r[,2])
+sum_r<-sum_r[order(sum_r[,2],decreasing = T),]
+
+edge_list_2<-melt(cell_2_go[sum_r[1:58,1],])
+edge_list_2<-edge_list_2[order(edge_list_2[,3],decreasing = T),]
+edge_list_2<-edge_list_2[1:500,]
+write.csv(edge_list_2,"edge_list_2.csv")
