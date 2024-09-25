f6a3a091e9a9978bce2d479b7f22fa68308e7e40
zhaopapers <zhaox_150314@163.com>
2024-09-25 16:12:22 +0800

    I am Prof. Dr. Long!

--- R/CITMIC.R
+++ R/CITMIC.R
@@ -1,14 +1,15 @@
 #'@title CITMIC
 #' @description The function "CITMIC" is used to identify cell infiltration in tumor microenvironment by calculating intercellular crosstalk.
-#'@param GEP An example gene expression profile
-#'@param weighted This parameter specifies whether to create a weighted graph for the cell-cell interaction network. If null, an unweighted graph is created, and the elements of the adjacency matrix indicate the number of edges between vertices. If true, a weighted graph is created(default: TRUE).
+#'@param GEP An example gene expression profile.
+#'@param weighted This parameter specifies whether to create a weighted graph for the cell crosstalk network. If null, an unweighted graph is created, and the elements of the adjacency matrix indicate the number of edges between vertices. If true, a weighted graph is created(default: TRUE).
 #'@param base Standardized log base of data for improving data distribution(default: 10).
 #'@param damping Restart the probability of the random-walk algorithm (default: 0.9).
 #'@param cl.cores The number of CPU cores applied to this task(default:1).
-#'@return A data frame containing the cell infiltration score for each sample
-#' @importFrom igraph graph.adjacency
+#'@param cell.type Preset the relevant cell type (e.g. if the solid tumour tissue does not contain 'HSC', it is better to remove it when we preset it.)
+#'@return A data frame containing the cell infiltration score for each sample.
+#' @importFrom igraph graph_from_adjacency_matrix
 #' @importFrom igraph V
-#' @importFrom igraph page.rank
+#' @importFrom igraph page_rank
 #' @importFrom stats median
 #' @importFrom stats p.adjust
 #' @importFrom stats pt
@@ -22,18 +23,19 @@
 #' @importFrom parallel parLapply
 #' @importFrom parallel stopCluster
 #' @importFrom parallel clusterEvalQ
-#'@usage CITMIC(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1)
+#'@usage CITMIC(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1,cell.type=NULL)
 #'@export
 #'@examples
 #' # Obtain the example data
 #' GEP<-GetData_CITMIC("GEP")
 #' # Run the function
-#' \donttest{lnScore<-CITMIC(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1)}
+#' \donttest{lnScore<-CITMIC(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1,cell.type=NULL)}
 
 
 
-CITMIC<-function(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1){
+CITMIC<-function(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1,cell.type=NULL){
 
+  GEP<-GEP
   cl<-makeCluster(cl.cores)
   clusterExport(cl,varlist = "GEP",envir = environment())
   clusterEvalQ(cl, {
@@ -98,8 +100,8 @@ CITMIC<-function(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1){
 
 
     adj.final<-as.matrix(result_cell)
-    graph = graph.adjacency(adj.final,mode=c("undirected"),weighted=weighted,add.rownames=T)
-    temp = page.rank(graph, vids=V(graph), directed=FALSE, damping=damping, weights=NULL)
+    graph = graph_from_adjacency_matrix(adj.final,mode=c("undirected"),weighted=weighted,add.rownames=T)
+    temp = page_rank(graph, vids=V(graph), directed=FALSE, damping=damping, weights=NULL)
     rank = temp$vector
     rank1 = as.matrix(rank)
 
@@ -121,6 +123,11 @@ CITMIC<-function(GEP,weighted = TRUE,base = 10,damping=0.90,cl.cores=1){
     return(y)
   })
   lnscore<-(cell_num-min(cell_num))/(max(cell_num)-min(cell_num))
+  if(is.null(cell.type)){
+    lnscore<-lnscore
+  }else{
+    lnscore[cell.type,]
+  }
   return(lnscore)
 }
 
